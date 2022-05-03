062f329f43dc3d28a6263ad8fecfd72624d21776
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2022-05-03 14:20:08 +0200

    change name

--- loadDataUtils.py
+++ loadDataUtils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-class load_data():
+class loadDataUtils():
     def __init__(self, train_path, test_path):
         self.train_path = train_path
         self.test_path = test_path
