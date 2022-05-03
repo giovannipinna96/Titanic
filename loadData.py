e91b0c77c1ccd7aa80ca01d1c5e79bb764106d9d
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2022-05-03 14:07:27 +0200

    object that store the data and some get function

---
+++ loadData.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+class load_data():
+    def __init__(self, train_path, test_path):
+        self.train_path = train_path
+        self.test_path = test_path
+
+        self.trainset, self.testset = self.__load(self.train_path, self.test_path)
+
+    def __load(self, train_path, test_path):
+        data_train = pd.read_csv(train_path)
+        data_test = pd.read_csv(test_path)
+        return data_train, data_test
+
+    def get_train_and_test(self):
+        return self.trainset, self.testset
+
+    def reload_data(self):
+        pass
+
+    def get_trainset(self):
+        return self.trainset
+
+    def get_testset(self):
+        return self.testset
+
+    def set_train_path(self, train_path):
+        self.train_path = train_path
+
+    def set_test_path(self, test_path):
+        self.test_path = test_path
